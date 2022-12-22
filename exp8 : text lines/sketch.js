cf68517b6a92400653540873a5b6e6d8277eb037
petlil <peterandrewliley@gmail.com>
2022-12-22 17:28:29 +0000

    Setup text display

--- exp8 : text lines/sketch.js
+++ exp8 : text lines/sketch.js
@@ -1,7 +1,16 @@
+let chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,^`'. "
+let numChars = 68; // 68 characters in the string
+let fontSize = 20;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+  textFont('Georgia', fontSize);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  fill(255);
+  
+  text(chars, 0, 0, textWidth(chars), fontSize);
 }
+
