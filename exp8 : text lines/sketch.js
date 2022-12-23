38f0646d456716df29e71d12fd1fb255619db64a
petlil <peterandrewliley@gmail.com>
2022-12-23 14:26:30 +0000

    Encapsulate text line behaviour into class

--- exp8 : text lines/sketch.js
+++ exp8 : text lines/sketch.js
@@ -1,14 +1,14 @@
 let chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,^`'. "
 let numChars = 68; // 68 characters in the string
 let fontSize = 20;
-let textLen = 0;
+let textLen = 0; // length of the string on the screen (in pixels)
 let numLines = 30;
 
 var x = 0;
 
 function setup() {
   createCanvas(800, 800);
-  textFont('Georgia', fontSize);
+  textFont('Calibri', fontSize);
   textLen = textWidth(chars);
   x = 0;
 }
@@ -25,6 +25,41 @@ function draw() {
     text(chars, x + (textLen * 2), y, textLen, fontSize);
   }
 
+  // progress line across screen
   x = x % textLen;
   x -= 3;
 }
+
+class line {
+  step = 0;
+  textWid = 0;
+  numRepeats = 0;
+
+  constructor(text, x, y, fontSize) {
+    this.text = text;
+    this.x = x;
+    this.y = y;
+    this.fontSize = fontSize;
+
+    this.textWid = textWidth(text);
+  }
+  
+  calculate() {
+    // get the number of times the line should be repeated
+    // to always fill the screen
+    numRepeats = int(width/textWid) + 1;
+
+    this.x += this.step;
+  }
+
+  render() {
+    for(i = 0; i < this.numRepeats; i++) {
+      text(text, x + (i * textLen), y, textLen, fontSize);
+    }
+  }
+
+  setStep(newStep) {
+    this.step = newStep;
+  }
+
+};
